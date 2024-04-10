c844df5b7f37dae6ed976426da267e7b75ef8c98
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-10 15:54:07 +0200

    Inserita nvabar in template.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,21 @@
 </script>
 
 <template>
-
+     <nav class="container">
+            <div class="row">
+            <ul class="main_nav">
+                <li>
+                    <a href="#">Donna</a>
+                </li>
+                <li>
+                    <a href="#">Uomo</a>
+                </li>
+                <li>
+                    <a href="#">Bambino</a>
+                </li>
+            </ul>
+            <img class="logo" src="boolean-logo.png" alt="Logo Azienda">
+        </nav>
 </template>
 
 <style>
