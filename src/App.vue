11bf5a0c0c407b23729a786a998447ef8ae8aaa0
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-11 17:07:41 +0200

    Inserito componente Main in template e in script.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <Navbar />
+  <Main />
   <Footer />
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
+import Main from './components/Main.vue';
 import Footer from './components/Footer.vue';
 
 export default {
   // Registro il componente.
   components: {
     Navbar,
+    Main,
     Footer
   },
   data() {
