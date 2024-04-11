99fce71221c903ce897b9c497d6eb84e04e4afc1
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-11 17:47:28 +0200

    In template inserito html. In style rimosso image card padding a 20.

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -25,8 +25,94 @@
                 </div>
 
             <!-- Fine Foto Modello 1 -->
+             <!-- Inizio Foto Modello 2 -->
 
+             <div class="image_card section_margin">
+                    <div class="image_position">
+                        <img class="image_size image_first" src="/2.webp" alt="Foto Modella Guess">
+                        <img class="image_size image_second" src="/2b.webp" alt="Foto Modella Guess">
+                        <div class="sconto">-30%</div>
+                        <div class="heart">&hearts;</div>
+                    </div>
+                    
+                    <div>Guess</div>
+                    <h2>Roses Tee</h2>
+                    <span class="price">20.99  &euro;</span> 
+                    <span class="old_price">29.99 &euro;</span>
+                </div>
 
+                <!-- Fine Foto Modello 2 -->
+
+                <!-- Inizio Foto Modello 3 -->
+
+                 <div class="image_card section_margin">
+                    <div class="image_position">
+                        <img class="image_size image_first" src="/3.webp" alt="Foto Modello Levi's">
+                        <img class="image_size image_second" src="/3b.webp" alt="Foto Mdello Levi's">
+                        <div class="sconto">-50%</div>
+                        <div class="sostenibilità">Sostenibilità</div>
+                        <div class="heart">&hearts;</div>
+                    </div>
+                    
+                    <div>Come zucchero filato</div>
+                    <h2>Voglia di colori pastello</h2>
+                    <span class="price">129.99 &euro;</span> 
+                    <span class="old_price">184.99 &euro;</span>
+                </div>
+
+                <!-- Fine Foto Modello 3 -->
+
+                <!-- Inizio Foto Modello 4 -->
+
+                 <div class="image_card section_margin">
+                    <div class="image_position">
+                        <img class="image_size image_first" src="/4.webp" alt="Foto Modello Levi's">
+                        <img class="image_size image_second" src="/4b.webp" alt="Foto Modello">
+                        <div class="sconto">-50%</div>
+                        <div class="sostenibilità">Sostenibilità</div>
+                        <div class="heart">&hearts;</div>
+                    </div>
+                    
+                    <div>Levi's</div>
+                    <h2>TEE UNISEX</h2>
+                    <span class="price">14.99 &euro;</span> 
+                    <span class="old_price">29.99 &euro;</span>
+                </div>
+
+                <!-- Fine modello 4 -->
+                
+                <!-- Inizio modello 5 -->
+
+                <div class="image_card section_margin">
+                    <div class="image_position">
+                        <img class="image_size image_first" src="/5.webp" alt="Foto Modello Levi's">
+                        <img class="image_size image_second" src="/5b.webp" alt="Foto Modello Levi's">
+                        <div class="heart">&hearts;</div>
+                    </div>
+                    
+                    <div>Maya deluxe</div>
+                    <h2>Stripe Bodice</h2>
+                    <span class="price">99.99 &euro;</span>
+                </div>
+
+                <!-- Fine modello 5 -->
+
+                <!-- Inizio Modello 6 -->
+
+                <div class="image_card section_margin">
+                    <div class="image_position">
+                        <img class="image_size image_first" src="/6.webp" alt="Foto Modello Levi's">
+                        <img class="image_size image_second" src="/6b.webp" alt="Foto">
+                        <div class="sostenibilità_no_margin">Sostenibilità</div>
+                        <div class="heart">&hearts;</div>
+                    </div>
+                    
+                    <div>Esprit</div>
+                    <h2>Maglione Black</h2>
+                    <span class="price">29.99 &euro;</span> 
+                </div>
+
+                <!-- Fine modello 6 -->
         </div>
     </div>
     </main>
@@ -35,10 +121,6 @@
 <style>
     .image_position {
     position: relative;
-}
-
-.image_card {
-    padding: 20px;
 }
 
 .image_card .image_second {
