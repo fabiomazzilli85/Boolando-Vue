30bf79b90518a6a373ef1c8c0f8659671c427392
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-11 15:37:06 +0200

    Componente Footer. Nel template ho aggiunto una classe. In style uniformato esteticamente componente con componente Navbar.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,4 +1,10 @@
 <script>
+export default {
+  created() {
+    console.log('Componente footer creato.');
+    // Essendo questo componente un footer, non saprei che script eseguire seS non un banalissimo console.log.
+  }
+}
 </script>
 
 <template>
@@ -6,7 +12,7 @@
     <div class="footer container background_footer">
         <div class="text">
             <h3 class="bottom_footer">Boolando Srl</h3>
-            <div class="footer_flex">
+            <div class="footer_flex footer-color">
                 <div>Informazioni legali</div>
                 <div>Informazioni sulla Privacy</div>
                 <div>Diritto di Recesso</div>
@@ -17,5 +23,30 @@
 </template>
 
 <style>
+h3 {
+    text-align: center;
+    padding: 20px;
+}
 
+footer {
+    font-family: Arial, Helvetica, sans-serif;
+    text-transform: uppercase;
+}
+
+.background_footer {
+    background-color: #8CB9BD;
+    width: 100%;
+
+}
+
+.footer_flex {
+    display: flex;
+    justify-content: space-between;
+    gap: 20px;
+    padding: 20px;
+} 
+
+.footer-color {
+    color: white;
+}
 </style>
