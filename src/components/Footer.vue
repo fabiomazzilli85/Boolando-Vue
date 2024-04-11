c2cdf0f95538570c4d3443d1f6698e2b607f8e3e
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-11 15:20:54 +0200

    Creato componente Footer con unn template all interno.

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<template>
+<footer>
+    <div class="footer container background_footer">
+        <div class="text">
+            <h3 class="bottom_footer">Boolando Srl</h3>
+            <div class="footer_flex">
+                <div>Informazioni legali</div>
+                <div>Informazioni sulla Privacy</div>
+                <div>Diritto di Recesso</div>
+            </div>
+        </div>
+    </div>
+</footer>
+</template>
+
+<style>
+
+</style>
